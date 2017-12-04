7339ebb9c356884f84b844ab8d37ca51a81ecc83
GameEatSleep <ammarb91@gmail.com>
2017-12-04 04:14:04 -0800

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
  # 895 automation script
 
- ajc -1.8 AspectSrc/dynamicTracer.java -outxml -outjar aspect.jar
+ ajc -1.8 AspectSrc/dynamicTracer.java -outjar aspect.jar
  
  cd jbox2d
  
@@ -11,22 +11,16 @@
  
  java -jar proguard.jar '@myconfig.pro'
  
- java -jar jshrink.jar jbox2d/jbox2d-testbed/target/jbox2d-testbed-2.3.1-SNAPSHOT.jar -overwrite -o snapshotj.jar -keep org.? -cp jbox2d/jbox2d-library/target/classes;jbox2d/jbox2d-serialization/target/classes
+ java -jar jshrink.jar jbox2d/jbox2d-testbed/target/jbox2d-testbed-2.3.1-SNAPSHOT.jar -overwrite -o snapshotj.jar -keep org.? -classpath "<java.home>/lib/rt.jar;jbox2d/jbox2d-library/target/classes;jbox2d/jbox2d-serialization/target/classes"
  
  cd unobfuscated
  
- mvn clean install
- 
- mvn exec:java
+ mvn clean install exec:java
  
  cd ../jShrink
  
- mvn clean install
- 
- mvn exec:java
+ mvn clean install exec:java
  
  cd ../proguard
  
- mvn clean install
- 
- mvn exec:java
+ mvn clean install exec:java
